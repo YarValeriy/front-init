4b41834897bf4206c692ea048b2c02b57588346d
YarValeriy <147444561+YarValeriy@users.noreply.github.com>
2024-02-24 15:07:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import mimetypes
 import urllib.parse
 import json
@@ -7,17 +8,17 @@ from datetime import datetime
 from pathlib import Path
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
-
 from jinja2 import Environment, FileSystemLoader
 
 BASE_DIR = Path()
 BUFFER_SIZE = 1024
 HTTP_PORT = 3000
-HTTP_HOST = '0.0.0.0'
-SOCKET_HOST = '127.0.0.1'
+HTTP_HOST = "0.0.0.0"
+SOCKET_HOST = "127.0.0.1"
 SOCKET_PORT = 5000
+json_file_path = "storage/data.json"
 
-jinja = Environment(loader=FileSystemLoader('templates'))
+jinja = Environment(loader=FileSystemLoader("templates"))
 
 
 class GoitFramework(BaseHTTPRequestHandler):
@@ -25,19 +26,19 @@ class GoitFramework(BaseHTTPRequestHandler):
     def do_GET(self):
         route = urllib.parse.urlparse(self.path)
         match route.path:
-            case '/':
-                self.send_html('index.html')
-            case '/message':
-                self.send_html('message.html')
+            case "/":
+                self.send_html("index.html")
+            case "/message":
+                self.send_html("message.html")
             case _:
                 file = BASE_DIR.joinpath(route.path[1:])
                 if file.exists():
                     self.send_static(file)
                 else:
-                    self.send_html('error.html', 404)
+                    self.send_html("error.html", 404)
 
     def do_POST(self):
-        size = self.headers.get('Content-Length')
+        size = self.headers.get("Content-Length")
         data = self.rfile.read(int(size))
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -45,26 +46,26 @@ class GoitFramework(BaseHTTPRequestHandler):
         client_socket.close()
 
         self.send_response(302)
-        self.send_header('Location', '/message')
+        self.send_header("Location", "/message")
         self.end_headers()
 
     def send_html(self, filename, status_code=200):
         self.send_response(status_code)
-        self.send_header('Content-Type', 'text/html')
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
-        with open(filename, 'rb') as file:
+        with open(filename, "rb") as file:
             self.wfile.write(file.read())
 
     def render_template(self, filename, status_code=200):
         self.send_response(status_code)
-        self.send_header('Content-Type', 'text/html')
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
 
-        with open('storage/db.json', 'r', encoding='utf-8') as file:
+        with open("storage/db.json", "r", encoding="utf-8") as file:
             data = json.load(file)
 
         template = jinja.get_template(filename)
-        message = None 
+        message = None
         html = template.render(blogs=data, message=message)
         self.wfile.write(html.encode())
 
@@ -72,11 +73,11 @@ class GoitFramework(BaseHTTPRequestHandler):
         self.send_response(status_code)
         mime_type, *_ = mimetypes.guess_type(filename)
         if mime_type:
-            self.send_header('Content-Type', mime_type)
+            self.send_header("Content-Type", mime_type)
         else:
-            self.send_header('Content-Type', 'text/plain')
+            self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        with open(filename, 'rb') as file:
+        with open(filename, "rb") as file:
             self.wfile.write(file.read())
 
 
@@ -88,7 +89,7 @@ def save_data_from_form(data):
         }
         timestamp = datetime.now().isoformat()
         new_data = {timestamp: parse_dict}
-        with open("storage/data.json", "r+", encoding="utf-8") as file:
+        with open(json_file_path, "r+", encoding="utf-8") as file:
             try:
                 existing_data = json.load(file)
             except json.JSONDecodeError:
@@ -101,6 +102,7 @@ def save_data_from_form(data):
         logging.error(f"ValueError: {err}")
     except OSError as err:
         logging.error(f"OSError: {err}")
+
 
 
 def run_socket_server(host, port):
@@ -130,9 +132,16 @@ def run_http_server(host, port):
         http_server.server_close()
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
-    
+if __name__ == "__main__":
+
+    if not os.path.exists(os.path.dirname(json_file_path)):
+        os.makedirs(os.path.dirname(json_file_path))
+    if not os.path.exists(json_file_path):
+        with open(json_file_path, "w", encoding="utf-8") as new_file:
+            new_file.write("{}")
+
+    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
+
     server = Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
     server.start()
 
